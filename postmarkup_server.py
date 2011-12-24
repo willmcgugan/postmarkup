2b4dff7aca2fb1a571a72b5d5faf0b9503a23efa
willmcgugan <willmcgugan@1483d188-dd31-0410-8476-e7d0cc56440d>
2011-12-24 14:56:05 +0000

    Fix

--- postmarkup_server.py
+++ postmarkup_server.py
@@ -5,13 +5,13 @@ You can find it on http://postmarkup.willmcgugan.com
 """
 
 import postmarkup
-from os.path import dirname, join
+from os.path import basename, dirname, join
 try:
     from fs.osfs import OSFS
 except ImportError:
     print "Get PyFilesystem from http://code.google.com/p/pyfilesystem/"
     raise
-
+import mimetypes
 
 def application(environ, start_response):
     fs = OSFS(join(dirname(__file__), "static"))
@@ -21,12 +21,13 @@ def application(environ, start_response):
     if path == "/getbbcode":
         bbcode = unicode(environ["wsgi.input"].read(), 'utf-8')
         html = postmarkup.render_bbcode(bbcode, clean=True)
-        start_response("200 OK", [])
+        start_response("200 OK", [("content-type", " text/html; charset=utf-8")])
         return [html]
+    mime_type, _encoding = mimetypes.guess_type(basename(path))
     if not fs.isfile(path):
         start_response("404 NOT FOUND", [])
         return ["Nobody here but us chickens: %s" % path]
-    start_response("200 OK", [])    
+    start_response("200 OK", [("Content-type", mime_type)])    
     return [fs.getcontents(path)]
     
         
